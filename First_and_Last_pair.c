683fec8d15d567ef37de9044f000670fedbaa0b5
SriLakshmiSoujanya <147716126+SriLakshmiSoujanya@users.noreply.github.com>
2023-12-27 22:43:35 +0530

    #include<stdio.h> int main() {     int i,n,j;     scanf("%d",&n);     int arr[n];     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0,j=n-1;i<j;i++,j--)     {         printf("%d %d ",arr[i],arr[j]);     }     if(n%2==1)     printf("%d %d",arr[j],0); }

---
+++ First_and_Last_pair.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,j;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0,j=n-1;i<j;i++,j--)
+    {
+        printf("%d %d ",arr[i],arr[j]);
+    }
+    if(n%2==1)
+    printf("%d %d",arr[j],0);
+}
